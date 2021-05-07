2091b21d11f7562662b47667d05c7153487efc76
Michael Frankham <michael.frankham@praemium.com>
2021-05-07 17:55:20 +1000

    hbs

--- webpack.config.js
+++ webpack.config.js
@@ -6,23 +6,11 @@ const path = require('path');
 const fs = require('fs');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { extendDefaultPlugins } = require('svgo');
 
 const environment = require('./configuration/environment');
-
-const templateFiles = fs.readdirSync(environment.paths.source)
-  .filter((file) => path.extname(file).toLowerCase() === '.html');
-
-const htmlPluginEntries = templateFiles.map((template) => new HTMLWebpackPlugin({
-  inject: true,
-  hash: false,
-  filename: template,
-  template: path.resolve(environment.paths.source, template),
-  favicon: path.resolve(environment.paths.source, 'images', 'favicon.ico'),
-}));
 
 module.exports = {
   entry: {
@@ -69,34 +57,15 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.hbs$/,
+        loader: 'handlebars-loader'
+      }
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'css/[name].css',
-    }),
-    new ImageMinimizerPlugin({
-      test: /\.(jpe?g|png|gif|svg)$/i,
-      minimizerOptions: {
-        // Lossless optimization with custom option
-        // Feel free to experiment with options for better result for you
-        plugins: [
-          ['gifsicle', { interlaced: true }],
-          ['jpegtran', { progressive: true }],
-          ['optipng', { optimizationLevel: 5 }],
-          [
-            'svgo',
-            {
-              plugins: extendDefaultPlugins([
-                {
-                  name: 'removeViewBox',
-                  active: false,
-                },
-              ]),
-            },
-          ],
-        ],
-      },
     }),
     new CleanWebpackPlugin({
       verbose: true,
@@ -122,6 +91,13 @@ module.exports = {
         },        
       ],
     }),
-  ].concat(htmlPluginEntries),
+    new HTMLWebpackPlugin({
+      inject: true,
+      hash: false,
+      filename: "index.html",
+      template: "./src/index.hbs",
+      favicon: path.resolve(environment.paths.source, 'images', 'favicon.ico'),
+    })
+  ],
   target: 'web',
 };
