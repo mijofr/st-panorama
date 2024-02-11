3b30c6010fd5508435b3bacd48495de0d4c020d4
Michael Frankahm <mikejf@gmail.com>
2024-02-11 18:55:21 +1100

    lintfix

--- src/js/app.js
+++ src/js/app.js
@@ -9,66 +9,64 @@ import supportsWebP from 'supports-webp';
 import * as SetupData from "./panoSetup.json";
 
 var viewer;
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
 
-    supportsWebP.then(supported => {
+	supportsWebP.then(supported => {
 
-        let imgExt = ".jpg";
+		let imgExt = ".jpg";
 
-        if (supported) {
-            window.useWebP = true;
-            imgExt = ".webp";
-        } else {
-            window.useWebP = false;
-        }
+		if (supported) {
+			window.useWebP = true;
+			imgExt = ".webp";
+		} else {
+			window.useWebP = false;
+		}
+
+		viewer = new PhotoSphereViewer.Viewer({
+			panorama: "./panorama-assets/panoramas/starmap" + imgExt,
+			container: 'photosphere',
+			// caption: 'Parc national du Mercantour <b>&copy; Damien Sorel</b>',
+			loadingImg: './images/loading.png',
+			defaultLat: 0.3,
+			touchmoveTwoFingers: false,
+			mousewheelCtrlKey: false,
+			autorotateDelay: 1,
+			autorotateSpeed: "0.5rpm",
+			panoData: {
+				fullWidth: 6144,
+				fullHeight: 3072,
+				croppedWidth: 6144,
+				croppedHeight: 3072,
+				croppedX: 0,
+				croppedY: 0,
+				poseHeading: 270, // 0 to 360
+				posePitch: 0, // -90 to 90
+				poseRoll: 0, // -180 to 180
+			},
+			canvasBackground: "#000000",
+			fisheye: -0.1,
+			navbar: false,
+			plugins: [[VisibleRangePlugin, { usePanoData: true }]]
+		});
 
 
-    
-        viewer = new PhotoSphereViewer.Viewer({
-            panorama: "./panorama-assets/panoramas/starmap" + imgExt,
-            container: 'photosphere',
-            // caption: 'Parc national du Mercantour <b>&copy; Damien Sorel</b>',
-            loadingImg: './images/loading.png',
-            defaultLat: 0.3,
-            touchmoveTwoFingers: false,
-            mousewheelCtrlKey: false,
-            autorotateDelay: 1,
-            autorotateSpeed: "0.5rpm",
-               panoData: {
-                fullWidth: 6144,
-                fullHeight: 3072,
-                croppedWidth: 6144,
-                croppedHeight: 3072,
-                croppedX: 0,
-                croppedY: 0,
-                poseHeading: 270, // 0 to 360
-                posePitch: 0, // -90 to 90
-                poseRoll: 0, // -180 to 180
-            },
-            canvasBackground: "#000000",
-            fisheye: -0.1,
-            navbar: false,
-            plugins: [[VisibleRangePlugin, {usePanoData: true}]]  
-          });
-    
-    
-          let hash = window.location.hash;
-          if (hash != undefined && hash.length > 0) {
-              let id = hash.substring(1);
-              console.log(id);
-              window.activateId(id);
-          }    
+		let hash = window.location.hash;
+		if (hash != undefined && hash.length > 0) {
+			let id = hash.substring(1);
+			console.log(id);
+			window.activateId(id);
+		}
 
-    });
+	});
 
-    this.setTimeout(() => {
-        let el = document.getElementById("innerBar");
-        if (el) {
-            el.className = "ready";
-        }
-    }, 1500);
-    
-    
+	this.setTimeout(() => {
+		let el = document.getElementById("innerBar");
+		if (el) {
+			el.className = "ready";
+		}
+	}, 1500);
+
+
 
 
 });
@@ -76,79 +74,77 @@ window.addEventListener("load", function(){
 
 window.activateId = (id) => {
 
-    let imgExt = ".jpg";
-    if (window.useWebP) {
-        imgExt = ".webp";
-    }
+	let imgExt = ".jpg";
+	if (window.useWebP) {
+		imgExt = ".webp";
+	}
 
-    let styleTag = document.getElementById("hTag");
+	let styleTag = document.getElementById("hTag");
 
-    styleTag.innerHTML = `
-        #${id} {
-            stroke: #c96046; paint-order: fill stroke markers;
-        }
-    `;
+	styleTag.innerHTML = `
+		#${id} {
+			stroke: #c96046; paint-order: fill stroke markers;
+		}
+	`;
 
 
-    let panoSetup = {
-        fullWidth: 2496,
-        fullHeight: 1536,
-        croppedWidth: 2496,
-        croppedHeight: 768,
-        croppedX: 0,
-        croppedY: 384,
-        poseHeading: 0, // 0 to 360
-        posePitch: 0, // -90 to 90
-        poseRoll: 0, // -180 to 180
-    };
+	let panoSetup = {
+		fullWidth: 2496,
+		fullHeight: 1536,
+		croppedWidth: 2496,
+		croppedHeight: 768,
+		croppedX: 0,
+		croppedY: 384,
+		poseHeading: 0, // 0 to 360
+		posePitch: 0, // -90 to 90
+		poseRoll: 0, // -180 to 180
+	};
 
-    for (let n of SetupData.default.panoDatas) {
-        if (n[0] == id) {
-            console.log
-            panoSetup = n[1];
-        }
-    }
+	for (let n of SetupData.default.panoDatas) {
+		if (n[0] == id) {
+			panoSetup = n[1];
+		}
+	}
 
-    let hasAlt = false;
-    for (let n of SetupData.default.alts) {
-        let alertButton = document.getElementById("alertButton");
-        if (n[0] == id) {
-            alertButton.className = "visible";
-            alertButton.setAttribute("ALT_LINK", n[1]);
-            hasAlt = true;
-        }
-    }
-    if (hasAlt == false) {
-        alertButton.className = null;
-    }
-    
-    let url = `./panorama-assets/panoramas/${id}${imgExt}`;
-    let opts = {
-        panoData: panoSetup
-    };
-    viewer.setPanorama(url, opts);
+	let hasAlt = false;
+	for (let n of SetupData.default.alts) {
+		let alertButton = document.getElementById("alertButton");
+		if (n[0] == id) {
+			alertButton.className = "visible";
+			alertButton.setAttribute("ALT_LINK", n[1]);
+			hasAlt = true;
+		}
+	}
+	if (hasAlt == false) {
+		alertButton.className = null;
+	}
 
-    if (viewer.isAutorotateEnabled()) {
-        viewer.stopAutorotate();
-    }  
-    
-    // var visibleRangePlugin = viewer.getPlugin(VisibleRangePlugin);
-    //visibleRangePlugin.setRangesFromPanoData();
+	let url = `./panorama-assets/panoramas/${id}${imgExt}`;
+	let opts = {
+		panoData: panoSetup
+	};
+	viewer.setPanorama(url, opts);
 
-}
+	if (viewer.isAutorotateEnabled()) {
+		viewer.stopAutorotate();
+	}
+
+	// var visibleRangePlugin = viewer.getPlugin(VisibleRangePlugin);
+	// visibleRangePlugin.setRangesFromPanoData();
+
+};
 
 window.alrtClicked = ($element) => {
-    let altlink = $element.getAttribute("alt_link");
-    if (altlink) {
-        window.activateId(altlink);
-    }
-}
+	let altlink = $element.getAttribute("alt_link");
+	if (altlink) {
+		window.activateId(altlink);
+	}
+};
 
 window.cclick = ($element) => {
-    let target = $element.target;
-    let id = target.id;
+	let target = $element.target;
+	let id = target.id;
 
-    window.activateId(id);
+	window.activateId(id);
 
-
-}
+};
